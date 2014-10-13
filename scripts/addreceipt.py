56c780c893c43a9d3130bb7328ea2e5d2b438fa9
jorenezra <tackeedot@gmail.com>
2014-10-13 18:51:07 +0400

    Added pythons for receipt

--- scripts/addreceipt.py
+++ scripts/addreceipt.py
@@ -6,7 +6,10 @@ def index(req,receipt_no):
 	receipt_no = cgi.escape(receipt_no)
 		
     x = doSql()
-    rets = x.execqry("select * from get_receipt('" + receipt_no + "');", False)
+	
+	amount = set.execqry("select birthday from personalinfo where userID = " + receipt_no + ";", False)[0][0]
+	
+    rets = x.execqry("select * from get_receipt('" + receipt_no + "', '" + amount + "');", False)
     result = []
     for ret in rets:
         stringed = map(str, ret)
