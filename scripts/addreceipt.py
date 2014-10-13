9dff355f660c4d127ffd94fb55d5608d36e2b624
jorenezra <tackeedot@gmail.com>
2014-10-13 19:56:20 +0400

    updated addreceipt

--- scripts/addreceipt.py
+++ scripts/addreceipt.py
@@ -5,16 +5,16 @@ from dosql import *
 def index(req,receipt_no):
 	receipt_no = cgi.escape(receipt_no)
 		
-    x = doSql()
+	x = doSql()
 	
-	amount = set.execqry("select birthday from personalinfo where userID = " + receipt_no + ";", False)[0][0]
+	amount = x.execqry("select amount from payBill where receiptNo = " + receipt_no + ";", False)[0][0]
 	
-    rets = x.execqry("select * from get_receipt('" + receipt_no + "', '" + amount + "');", False)
-    result = []
-    for ret in rets:
-        stringed = map(str, ret)
-        result.append(stringed)
+	rets = x.execqry("select * from get_receipt('" + str(receipt_no) + "', '" + str(amount) + "');", False)
+	result = []
+	for ret in rets:
+		stringed = map(str, ret)
+		result.append(stringed)
 
-    return json.dumps(result)
+	return json.dumps(result)
 		
 	
