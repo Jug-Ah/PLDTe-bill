4ec83e42d43e54580f31003a24d7fbab7a15f96a
jorenezra <tackeedot@gmail.com>
2014-10-13 20:18:28 +0400

    Fixed print receipt

--- scripts/printreceipt.py
+++ scripts/printreceipt.py
@@ -5,13 +5,13 @@ from dosql import *
 def index(req,receipt_no):
 	receipt_no = cgi.escape(receipt_no)
 		
-    x = doSql()
-    rets = x.execqry("select * from get_receipt('" + receipt_no + "');", False)
-    result = []
-    for ret in rets:
-        stringed = map(str, ret)
-        result.append(stringed)
-
-    return json.dumps(result)
+	x = doSql()
+	rets = x.execqry("select * from get_receipt('" + receipt_no + "');", False)
+	result = []
+	for ret in rets:
+		stringed = map(str, ret)
+		result.append(stringed)
+		
+	return json.dumps(result)
 		
 	
