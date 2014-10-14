e80329b77b0b58168e8e507701e9fc4764762ff9
Silver Serate <silverserate64@gmail.com>
2014-10-14 02:08:45 -0700

    Added show latest bill, receipt on GUI.
    - It now displays current bill before amount input and payment.
    - it now displays receipt after payment

--- javascript/globs.js
+++ javascript/globs.js
@@ -2,6 +2,28 @@
 var siteloc = "http://localhost/PLDTe-bill";
 var scriptloc = "/scripts/";
 
+$(document).ready(function () {
+
+    $('#form-input').validate({
+        rules: {
+            PLDTaccountnumber: {                
+                required: true
+            },
+            BANKaccountnumber: {
+              required: true
+            
+            },
+        },        
+        highlight: function (element) {
+            $(element).closest('.controls').addClass('has-error');
+        },
+        success: function (element) {
+            $(element).closest('.controls').removeClass('has-error');
+        }
+    }); 
+        
+});
+
 
 function getBill()
 {  	
@@ -36,17 +58,27 @@ function paybill()
               if (isNaN(res)){
                 alert(res)
               }
-              else {
-                alert("OK")
-                accountnumdisplay = ' <h3 id="pldt-acc-display" class="panel-title">PLDT Account Number: ';
-                accountnumdisplay += res[0][0];
-                accountnumdisplay += '</h3>'; 
-                $("#pldtnum").html(accountnumdisplay);    
-                $("#current-bill").html("Latest Bill: P" + res[0][1]);
+              else {                
+                sessionStorage.receipt = res                
+                $("#receipt-page").attr("class","tab-pane fade in active");
+                $("#bill-page").attr("class","tab-pane fade");  
+                $.ajax({
+                  url: siteloc + scriptloc + "getbill.py",
+                  data: {pldtNum:$("#PLDTaccountnumber").val()},
+                  dataType: 'json',
+                  async: true,
+                  success: function (res) {                         
+                    $("#pldtnum").html("Account Number: " + res[0][0]);                
+                    $("#latest-bill").html("Latest Bill: P" + res[0][1]);
+                  }
+                });                
+                
+
+                makereceipt();
               }
             }
     });
-  makereceipt();
+  
 }
 
 function makereceipt()
@@ -58,26 +90,26 @@ function makereceipt()
       async: true,
       success: function (res) {
           console.log("Receipt created");
-              }
+          printreceipt()
+      }
     });
 }
 
 function printreceipt()
 {
   $.ajax({ 
-      url: siteloc + scriptloc + "addreceipt.py",
+      url: siteloc + scriptloc + "printreceipt.py",
       data: {receipt_no:sessionStorage.receipt},
     dataType: 'json',
       async: true,
-      success: function (res) {
-          var receiptNo = res[0][0];
-                  var timedate = res[0][1];
-          var amount = res[0][2];
+      success: function (res) {        
+        var receiptNo = res[0][0];
+        var timedate = res[0][1];
+        var amount = res[0][2];
 
-
-                  $("#receiptNo").html(receiptNo);
-                  $("#timedate").html(timedate);
-          $("#amount").html(amount);
+        $("#receiptNo").html("Receipt #: " + receiptNo);
+        $("#timedate").html("Date and time of payment: " + timedate);
+        $("#amount").html("Amount Paid: " + amount);
               }
     });
 }
